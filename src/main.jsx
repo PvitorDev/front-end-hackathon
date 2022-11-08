c0e7ac879e23238619802d586f70e0f717967b3d
PvitorDev <pvitor.dev@gmail.com>
2022-11-08 00:26:45 -0300

    feat: Modal Login + Cadastro e integração com backend

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { Login } from './components/modalLogin'
 import './index.css'
-
+import "react-toastify/dist/ReactToastify.css"; 
+import { ToastContainer } from "react-toastify";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ToastContainer autoClose={3000} />
+    <Login />
   </React.StrictMode>
 )
