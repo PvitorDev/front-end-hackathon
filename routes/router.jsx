a2ef321673adb781c3ff9384ee04870024b5339c
PvitorDev <pvitor.dev@gmail.com>
2022-11-09 22:14:41 -0300

    fix: routes

--- routes/router.jsx
+++ routes/router.jsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { Login } from '../src/components/modalLogin';
 import { getLocalItem } from '../src/utils/localStorage';
 import {Home} from '../src/pages/Home'
+import {TabelaConteudos} from '../src/components/Tabelas/Conteudos/FullstackConteudos'
 function ProtectedRoutes({ children }) {
     const isAuthenticated = getLocalItem('token');
 
@@ -15,9 +16,9 @@ export function MainRoutes() {
             <Route path='/' element={
                 <Home/>}
             />
-            {/* <Route path='/tabelaConteudos' element={
+            {<Route path='/tabelaConteudos' element={
                     <TabelaConteudos />}
-            /> */}
+            />}
         <Route path='/home' element={<Home/>} />
         </Routes>
 
